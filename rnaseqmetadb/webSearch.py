3440aa36295bdbd3fa750279df6c8fbaf46bbf2f
Zhengyu Guo <zhengyuguo@tamu.edu>
2015-07-02 15:39:41 -0500

    changes

--- rnaseqmetadb/webSearch.py
+++ rnaseqmetadb/webSearch.py
@@ -14,15 +14,13 @@ class PageClass:
 
 
 def getAccessionID(keyword):
-	## get xml file given keyword
-	url = "http://www.ebi.ac.uk/arrayexpress/xml/v2/experiments?query=exptype%3ARNA-seq+organism%3A\"Mus+musculus\"" + keyword
+	#url = "http://www.ebi.ac.uk/arrayexpress/xml/v2/experiments?query=exptype%3ARNA-seq+organism%3A\"Mus+musculus\"" + keyword
+	url = 'http://www.ebi.ac.uk/arrayexpress/xml/v2/experiments?keywords='+keyword+'&organism=Mus+musculus&exptype%5B%5D="RNA-seq"&exptype%5B%5D="sequencing+assay"&array='
 	html = PageClass(url).read_page()
 
-	## get accession_id
 	find_re = re.compile(r'<accession>(.*?)</accession>')
 	AccessionIDs= [x for x in find_re.findall(html)]  
 
-	## filter out invalid accession id
 	AccessionIDs = filter( lambda x: re.match(".-.*-.*" ,x), AccessionIDs)
 	return AccessionIDs
 
